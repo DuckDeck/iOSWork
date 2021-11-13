5708cbc75e88196c024e1a699b075e87dcce2adf
DuckDeck <3421902@qq.com>
2021-11-13 18:47:43 +0800

    加入一些布局

---
+++ iOSWork/UI_Layout/Layout/FlowLayoutViewController.swift
@@ -0,0 +1,50 @@
+//
+//  FlowLayoutViewController.swift
+//  iOSWork
+//
+//  Created by Stan Hu on 2021/11/13.
+//
+
+import UIKit
+class FlowLayoutViewController: UIViewController {
+
+    var vCol:UICollectionView!
+    var heights = [Double]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for _ in 0..<80{
+            var i = Double(arc4random() % 100 )
+            if i < 20{
+                i = i + 20
+            }
+            heights.append(i)
+        }
+        let layout = FlowLayout(columnCount: 3, columnMargin: 8) { [weak self](index) -> Double in
+           return self!.heights[index.row]
+        }
+
+        vCol = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        vCol.backgroundColor = UIColor.white
+        vCol.delegate = self
+        vCol.dataSource = self
+        vCol.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        view.addSubview(vCol)
+    }
+    
+
+   
+}
+
+extension FlowLayoutViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 80
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = UIColor.random
+        return cell
+    }
+    
+    
+}
--- iOSWork/UI_Layout/UIMenuViewController.swift
+++ iOSWork/UI_Layout/UIMenuViewController.swift
@@ -5,10 +5,71 @@
 //  Created by Stan Hu on 2021/10/24.
 //
 
-import Foundation
+import UIKit
 class UIMenuViewController:BaseViewController{
+    var arrData = ["无限滚动的横向Table","样式Table","优化的Table","流式布局","GRID 布局","自己适应高度 Table"]
+    var tbMenu = UITableView()
+    var isHooked = false
+
     override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "多媒体&硬件"
+
         view.backgroundColor = UIColor.white
+        tbMenu.dataSource = self
+        tbMenu.delegate = self
+        tbMenu.tableFooterView = UIView()
+        view.addSubview(tbMenu)
+        tbMenu.snp.makeConstraints { (m) in
+            m.edges.equalTo(0)
+        }
+    }
+
+}
+extension UIMenuViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil{
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = arrData[indexPath.row]
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        switch indexPath.row {
+        case 0:
+            let vc = HorizontalTableViewController()
+            vc.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(vc, animated: true)
+        case 1:
+            let vc = StyleTableViewController()
+            vc.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(vc, animated: true)
+        case 2:
+            let vc = OptimizeTableViewController()
+            vc.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(vc, animated: true)
+        case 3:
+            let vc = FlowLayoutViewController()
+            vc.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(vc, animated: true)
+        case 4:
+            let vc = GridViewController()
+            vc.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(vc, animated: true)
+        case 5:
+            let vc = SnapkitTableViewController()
+            vc.hidesBottomBarWhenPushed = true
+            navigationController?.pushViewController(vc, animated: true)
+
+        default:
+            break
+        }
     }
 
 }
