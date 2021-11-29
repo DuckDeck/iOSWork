40a6dc768286895b70abbac01e77c48178f49cd2
shadowedge <3421902@qq.com>
2021-11-29 20:13:02 +0800

    加入图片浏览器

--- Library/View/ImageBrowserViewController.swift
+++ Library/View/ImageBrowserViewController.swift
@@ -25,7 +25,7 @@ class ImageBrowserViewController: UIViewController {
         layout.scrollDirection = .horizontal
         layout.itemSize = UIScreen.main.bounds.size
         layout.minimumLineSpacing = 0 //这个很重要，不然两个分页之间有空隙导致后面的页面偏见右
-        vc = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        vc = UICollectionView(frame: CGRect(x: 0, y: NavigationBarHeight, width: ScreenWidth, height: ScreenHeight - NavigationBarHeight), collectionViewLayout: layout)
         vc.isPagingEnabled = true
         vc.register(ImageBrowserCell.self, forCellWithReuseIdentifier: "ImageBrowserCell")
         vc.delegate = self
@@ -90,37 +90,52 @@ extension ImageBrowserViewController:UICollectionViewDelegate,UICollectionViewDa
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageBrowserCell", for: indexPath) as! ImageBrowserCell
         let img = arrImages![indexPath.row]
-        cell.img.setImg(url: img)
+        cell.setImg(url: img)
         cell.tapBlock = {[weak self](view:UIView) in
             self?.dismiss(animated: true, completion: nil)
         }
         return cell
     }
     
-    //func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //这里我就用关闭了,没有用
-        //dismiss(animated: true, completion: nil)
-    //}
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if let imgCell = cell as? ImageBrowserCell{
+            if imgCell.sc.zoomScale != 1{
+                imgCell.sc.zoomScale = 1
+            }
+        }
+    }
 }
 
 class ImageBrowserCell: UICollectionViewCell,UIScrollViewDelegate {
     let sc = UIScrollView()
     let img = UIImageView()
     var tapBlock:((_ view:UIView)->Void)?
+    
+    func setImg(url:String){
+        img.setImg(url: url, completed: { img in
+            self.img.size = CGSize(width: ScreenWidth - 30, height: (ScreenWidth - 30) / (img.size.width / img.size.height))
+        }, placeHolder: nil)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         contentView.addSubview(sc)
-        sc.snp.makeConstraints { (m) in
-            m.left.right.equalTo(0)
-            m.top.equalTo(20)
-            m.lastBaseline.equalTo(-20)
-        }
+        sc.frame = CGRect(x: 0, y: 0, width: ScreenWidth, height: ScreenHeight - 100)
+        sc.center = contentView.center
         sc.backgroundColor = UIColor.clear
         sc.isUserInteractionEnabled = true
         sc.delegate = self
         sc.minimumZoomScale = 1
-        sc.maximumZoomScale = 3
+        sc.maximumZoomScale = 4
+        sc.layer.borderColor = UIColor.yellow.cgColor
+        sc.layer.borderWidth = 1
+        sc.scrollsToTop = false
+        sc.alwaysBounceVertical = false
+        sc.canCancelContentTouches = true
+        sc.delaysContentTouches = false
+        sc.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        sc.isMultipleTouchEnabled = true
         sc.addSubview(img)
         img.contentMode = .scaleAspectFit
         img.isUserInteractionEnabled = true
@@ -131,15 +146,12 @@ class ImageBrowserCell: UICollectionViewCell,UIScrollViewDelegate {
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(imgDoubleTap(ges:)))
         doubleTap.numberOfTapsRequired = 2
         doubleTap.numberOfTouchesRequired = 1
-        img.addGestureRecognizer(tap)
-        tap.require(toFail: doubleTap) //Issue 只能识别单击手势，双击不能识别
-        
-        img.snp.makeConstraints { (m) in
-            m.center.equalTo(sc)
-            m.width.equalTo(ScreenWidth - 30)
-            m.height.equalTo(ScreenHeight - 60)
-        }
-        
+        img.addGestureRecognizer(doubleTap)
+
+        img.layer.borderColor = UIColor.red.cgColor
+        img.layer.borderWidth = 1
+        img.frame = CGRect(x: 0, y: 0, width: ScreenWidth - 15, height: ScreenWidth - 15)
+        img.center = sc.center
     }
     
     @objc func imgTap(ges:UIGestureRecognizer)  {
@@ -150,10 +162,10 @@ class ImageBrowserCell: UICollectionViewCell,UIScrollViewDelegate {
     
     @objc func imgDoubleTap(ges:UIGestureRecognizer)  {
         if sc.zoomScale > 1.5{
-            sc.zoomScale = 1
+            sc.setZoomScale(1.0, animated: true)
         }
         else{
-            sc.zoomScale = 3
+            sc.setZoomScale(3.0, animated: true)
         }
     }
     
@@ -166,6 +178,12 @@ class ImageBrowserCell: UICollectionViewCell,UIScrollViewDelegate {
         let offsetY = sc.bounds.size.height > sc.contentSize.height ? (sc.bounds.size.height - sc.contentSize.height) * 0.5 : 0.0
         img.center = CGPoint(x: sc.contentSize.width * 0.5 + offsetX, y: sc.contentSize.height * 0.5 + offsetY)
     }
+    
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+        scrollView.contentInset = UIEdgeInsets.zero
+    }
+    
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
