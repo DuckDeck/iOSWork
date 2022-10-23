bca38a4fb533fb15c51aed00ca430b8ca83226ee
DuckDeck <3421902@qq.com>
2022-10-23 14:44:45 +0800

    process code

--- iOSWork/AppDelegate.swift
+++ iOSWork/AppDelegate.swift
@@ -18,15 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         //这里的代码目前用不了，不知为何
-       // UncaughtExceptionHandler.installUncaughtExceptionHandler()
+        // UncaughtExceptionHandler.installUncaughtExceptionHandler()
         //
         
         let errs = Store.AppErrors.Value
         for item in errs{
             print(item.name)
         }
-
-//        Bugly.start(withAppId: "", config: BuglyConfig())
+        
+        //        Bugly.start(withAppId: "", config: BuglyConfig())
         
         let config = BuglyConfig()
         config.debugMode = true
@@ -42,48 +42,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             errors.append(err)
             Store.AppErrors.Value = errors
         }
-        
+        registerSignalHandler()
         let log = SwiftyBeaver.self
         
         let dir = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.ShadowEdge.iOSProject")!.appendingPathComponent("logs")
         if !FileManager.default.fileExists(atPath: dir.path){
-           try?  FileManager.default.createDirectory(at: dir, withIntermediateDirectories: true)
+            try?  FileManager.default.createDirectory(at: dir, withIntermediateDirectories: true)
         }
         let url = dir.appendingPathComponent("\(DateTime.now.dateString).log")
         
         
         let console = ConsoleDestination()  // log to Xcode Console
         let file = FileDestination(logFileURL: url)  // log to default swiftybeaver.log file
-//        let cloud = SBPlatformDestination(appID: "foo", appSecret: "bar", encryptionKey: "123") // to cloud
-//
-//        // use custom format and set console output to short time, log level & message
-//        console.format = "$DHH:mm:ss$d $L $M"
-//        // or use this for JSON output: console.format = "$J"
-//
-//        // add the destinations to SwiftyBeaver
+        //        let cloud = SBPlatformDestination(appID: "foo", appSecret: "bar", encryptionKey: "123") // to cloud
+        //
+        //        // use custom format and set console output to short time, log level & message
+        //        console.format = "$DHH:mm:ss$d $L $M"
+        //        // or use this for JSON output: console.format = "$J"
+        //
+        //        // add the destinations to SwiftyBeaver
         
         log.addDestination(console)
         log.addDestination(file)
-//        log.addDestination(cloud)
-//
-//        // Now let’s log!
+        //        log.addDestination(cloud)
+        //
+        //        // Now let’s log!
         log.verbose("not so important")  // prio 1, VERBOSE in silver
         log.debug("something to debug")  // prio 2, DEBUG in green
-//        log.info("a nice information")   // prio 3, INFO in blue
-//        log.warning("oh no, that won’t be good")  // prio 4, WARNING in yellow
-//        log.error("ouch, an error did occur!")  // prio 5, ERROR in red
-
-
+        //        log.info("a nice information")   // prio 3, INFO in blue
+        //        log.warning("oh no, that won’t be good")  // prio 4, WARNING in yellow
+        //        log.error("ouch, an error did occur!")  // prio 5, ERROR in red
         
-
+        
+        
+        
         // Override point for customization after application launch.
-
         
-//        let exception = NSException(name: NSExceptionName(rawValue:"arbitrary"), reason:"arbitrary reason", userInfo: nil)
-//            exception.raise()
-       
-//        var arr = [1,3,4,5]
-//        arr.insert(9, at: 10)
+        
+//                let exception = NSException(name: NSExceptionName(rawValue:"arbitrary"), reason:"arbitrary reason", userInfo: nil)
+//                    exception.raise()
+        
+        //        var arr = [1,3,4,5]
+        //        arr.insert(9, at: 10)
         return true
     }
     
@@ -106,5 +106,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         hookInstanceMethod(cls: WKWebView.self, originalSelector: #selector(WKWebView.load(_:)), swizzleSelector: #selector(WKWebView.ggLoad(_:)))
     }
     
+    
+
+    func registerSignalHandler() {
+        func SignalExceptionHandler(signal:Int32) -> Void {
+            var mstr = String()
+            //增加错误信息
+            for symbol in Thread.callStackSymbols {
+                mstr = mstr.appendingFormat("%@\r\n", symbol)
+            }
+            let log = SwiftyBeaver.self
+            log.error("异常崩溃报告:\(mstr)")
+            exit(signal)
+        }
+        
+//        signal(SIGABRT, SignalExceptionHandler)
+//        signal(SIGSEGV, SignalExceptionHandler)
+//        signal(SIGBUS, SignalExceptionHandler)
+//        signal(SIGTRAP, SignalExceptionHandler)
+//        signal(SIGILL, SignalExceptionHandler)
+        
+        signal(SIGABRT, SignalExceptionHandler)     //程序终止命令终止信号
+        signal(SIGSEGV, SignalExceptionHandler)     //程序试图访问未分配给自己的内存, 或试图往没有写权限的内存地址写数据.
+        signal(SIGBUS, SignalExceptionHandler)      //非法地址, 包括内存地址对齐(alignment)出错。比如访问一个四个字长的整数, 但其地址不是4的倍数。它与SIGSEGV的区别在于后者是由于对合法存储地址的非法访问触发的(如访问不属于自己存储空间或只读存储空间)。
+        signal(SIGTRAP, SignalExceptionHandler)
+        signal(SIGILL, SignalExceptionHandler)  //程序非法指令信号
+        signal(SIGQUIT,SignalExceptionHandler)  //和SIGINT类似, 但由QUIT字符(通常是Ctrl-)来控制. 进程在因收到SIGQUIT退出时会产生core文件, 在这个意义上类似于一个程序错误信号。
+        signal(SIGHUP,SignalExceptionHandler)   //程序终端终止信号
+        signal(SIGINT,SignalExceptionHandler)   //程序键盘终止信号
+        signal(SIGFPE,SignalExceptionHandler)   //在发生致命的算术运算错误时发出. 不仅包括浮点运算错误, 还包括溢出及除数为0等其它所有的算术的错误。
+        signal(SIGPIPE,SignalExceptionHandler)  //管道破裂。这个信号通常在进程间通信产生，比如采用FIFO(管道)通信的两个进程，读管道没打开或者意外终止就往管道写，写进程会收到SIGPIPE信号。此外用Socket通信的两个进程，写进程在写Socket的时候，读进程已经终止。
+   
+    }
 }
 
